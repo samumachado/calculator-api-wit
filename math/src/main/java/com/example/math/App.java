bacfe6116165fc74113810c2bcac4d5be5d17df4
samu <sebmachado2002@gmail.com>
2025-06-27 14:35:53 +0100

    fix

--- math/src/main/java/com/example/math/App.java
+++ math/src/main/java/com/example/math/App.java
@@ -1,4 +1,4 @@
-package com.example.calculator;
+package com.example.math;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
